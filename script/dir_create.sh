e3031048cd8550087809c677d8e4f5550347ff8a
marutyan76 <city.mati76@gmail.com>
2018-08-07 11:57:21 +0900

    編集中

--- script/dir_create.sh
+++ script/dir_create.sh
@@ -5,15 +5,15 @@ mkdir -p /var/html/flv
 chown nobody. /var/www/html/flv
 
 # nginx.conf source conf copy
-cp ../source/nginx_rtmp.conf /usr/local/nginx/conf/nginx.conf
+cp source/nginx_rtmp_source.conf /usr/local/nginx/conf/nginx.conf
 
 # nginx service conf copy
-cp ../source/live_nginx_source.service /usr/lib/systemd/system/live_nginx.service
+cp source/live_nginx_source.service /usr/lib/systemd/system/live_nginx.service
 
 # nginx service start&enable on
 systemctl start live_nginx.service
 systemctl enable live_nginx.service
 
 # Prepare players
-cp ../source/hp_hls_source.html /usr/local/nginx/html/hp_hls.html
-cp ../source/hp_dash_source.html /usr/local/nginx/html/hp_dash.html
+cp source/hp_hls_source.html /usr/local/nginx/html/hp_hls.html
+cp source/hp_dash_source.html /usr/local/nginx/html/hp_dash.html
